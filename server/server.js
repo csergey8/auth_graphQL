727cdc99224dfbd5ac3bf853466ef8a5b943f676
csergey8 <c.sergey@protonmail.com>
2018-03-30 12:04:33 +0300

    added threee mutaions : sighup, logout, login

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://user:110488@ds113586.mlab.com:13586/authdb';
 
 // Mongoose's built in promise library is deprecated, replace it with ES2015 Promise
 mongoose.Promise = global.Promise;
